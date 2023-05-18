a0bbf6a3143590761b1ee2b579a02c95791d4f63
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-18 19:21:02 +0300

    MangmentEmployee.java

--- demo7/src/main/java/com/example/demo/Model/MangmentEmployee.java
+++ demo7/src/main/java/com/example/demo/Model/MangmentEmployee.java
@@ -1,87 +1,37 @@
-package com.example.demo.Controller;
-import com.example.demo.Model.ManagementEmployee;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.validation.annotation.Validated;
+
 import java.util.Date;
 
-@RestController
-@RequestMapping("/api/v1/Management")
-public class ManagementEmployeeController {
-    ArrayList<ManagementEmployee> managementEmployees = new ArrayList();
+@AllArgsConstructor
+@Data
+@Validated
+public class ManagementEmployee {
 
-    @GetMapping("/get")
-    public String getAll() {
-        return managementEmployees.toString();
-    }
+    @NotNull(message = "Cannot be null")
+    private int id;
 
+    @NotEmpty(message = "Cannot be null")
+    @Length(min = 6)
+    private String name;
 
-    @PostMapping("/add")
-    public String add(@RequestBody @Valid ManagementEmployee managementEmployee) {
-        int count = 1;
-        for (int i = 0; i < managementEmployees.size(); i++) {
-            count++;
-        }
-        managementEmployee.setId(count);
-        managementEmployees.add(managementEmployee);
-        return managementEmployee.toString();
-    }
+    @NotNull(message = "Cannot be null")
+    @NumberFormat
+    @Min(value = 50)
+    private int age;
 
-    @PutMapping("/update/{id}")
-    public String put(@PathVariable int id, @Valid @RequestBody ManagementEmployee managementEmployee) {
-        int index = id - 1;
-        if (id == 0) {
-            return ("Pls enter valid number");
-        }
-        if (index != managementEmployees.size() - 1 && index > managementEmployees.size() - 1) {
-            return ("this id is not found");
-        } else {
-            int Id = managementEmployees.get(index).getId();
-            managementEmployee.setId(Id);
-            managementEmployees.set(index, managementEmployee);
-            return "Success";
-        }
-    }
+    @NotEmpty(message = "Cannot be null")
+    private String position;
 
-    @DeleteMapping("/delete/{id}")
-    public String delete(@PathVariable int id) {
-        if (id != managementEmployees.size() - 1 && id > managementEmployees.size() - 1) {
-            return ("this Id is not found");
-        }
-        managementEmployees.remove(id);
-        return "Success";
-    }
+    @AssertFalse
+    private boolean onLeave;
 
-    @PutMapping("/vacationDiscount/{id}")
-    public String vacationDiscount(@PathVariable int id, @RequestParam int vacation) {
-        int index = id - 1;
-        if (id == 0) {
-            return ("Pls enter valid number");
-        }
-        if (index != managementEmployees.size() - 1 && index > managementEmployees.size() - 1) {
-            return ("this id is not found");
-        } else {
-            int Id = managementEmployees.get(index).getId();
-            String name = managementEmployees.get(index).getName();
-            int age = managementEmployees.get(index).getAge();
-            String position = managementEmployees.get(index).getPosition();
-            Date employmentYear = managementEmployees.get(index).getEmploymentYear();
-            int annualLeave = managementEmployees.get(index).getAnnualLeave();
-            if(managementEmployees.get(index).isOnLeave()){
-                return ("BadRequest");
-            } else if (annualLeave == 0) {
-                return ("BadRequest");
-            }else {
-                managementEmployees.get(index).setId(Id);
-                managementEmployees.get(index).setName(name);
-                managementEmployees.get(index).setAge(age);
-                managementEmployees.get(index).setPosition(position);
-                managementEmployees.get(index).setOnLeave(true);
-                managementEmployees.get(index).setEmploymentYear(employmentYear);
-                managementEmployees.get(index).setAnnualLeave(annualLeave - vacation);
-                managementEmployees.set(index, managementEmployees.get(index));
-                return "Success";
-            }
-        }
-    }
+    @FutureOrPresent(message = "Cannot be null")
+    private Date employmentYear;
+
+    @NotNull(message = "Cannot be null")
+    @NumberFormat
+    private int annualLeave;
 }
